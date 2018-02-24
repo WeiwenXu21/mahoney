34be60336ca9c4797dd2b175c12ed6192f6ff2c1
Chris Barrick <cbarrick1@gmail.com>
2018-02-23 19:59:14 -0500

    Remove unused constants

--- mahoney/io.py
+++ mahoney/io.py
@@ -9,19 +9,6 @@ import skimage as ski
 import skimage.measure
 
 
-TRAIN_SETS = [
-    '00.00', '00.01', '00.02', '00.03', '00.04', '00.05', '00.06', '00.07',
-    '00.08', '00.09', '00.10', '00.11', '01.00', '01.01', '02.00', '02.01',
-    '03.00', '04.00', '04.01'
-]
-
-
-TEST_SETS = [
-    '00.00.test', '00.01.test', '01.00.test', '01.01.test', '02.00.test',
-    '02.01.test', '03.00.test', '04.00.test', '04.01.test'
-]
-
-
 def load_instance(path, imread=None, preprocess=None):
     '''Reads a collection of image files into a dask array.
 

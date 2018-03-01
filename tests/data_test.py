6f0252a74b21dfccad82f991c0fbd171d9e7bd8c
Chris Barrick <cbarrick1@gmail.com>
2018-02-28 19:53:14 -0500

    Add gridsearch test and tests for metadata

--- tests/data_test.py
+++ tests/data_test.py
@@ -9,7 +9,7 @@ import mahoney.data as data
 def test_neurofinder():
     # We take the fitst 8 blocks of 256 frames from video '01.00'.
     # Video '01.00' has 2250 frames at a pixel resolution of 512x512.
-    ds = data.Neurofinder('./data', n=256, stop=8, subset=['01.00'])
+    ds = data.load_dataset('./data', n=256, stop=8, subset=['01.00'])
     assert len(ds) == 8
 
     # We can successfully iterate over the dataset.
@@ -20,12 +20,13 @@ def test_neurofinder():
         assert ds[i]['y'].dtype == 'int64'
         assert isinstance(ds[i]['x'], da.Array)
         assert isinstance(ds[i]['y'], np.ndarray)
+        assert isinstance(ds[i]['meta'], dict)
 
 
 def test_torchify():
     # We take the fitst 8 blocks of 256 frames from video '01.00'.
     # Video '01.00' has 2250 frames at a pixel resolution of 512x512.
-    ds = data.Neurofinder('./data', n=256, stop=8, subset=['01.00'])
+    ds = data.load_dataset('./data', n=256, stop=8, subset=['01.00'])
     ds = data.Torchify(ds)
     assert len(ds) == 8
 
@@ -39,12 +40,13 @@ def test_torchify():
         assert y.dtype == 'int64'
         assert isinstance(x, np.ndarray)
         assert isinstance(y, np.ndarray)
+        assert isinstance(code, str)
 
 
 def test_dataloader():
     # We take the fitst 8 blocks of 256 frames from video '01.00'.
     # Video '01.00' has 2250 frames at a pixel resolution of 512x512.
-    ds = data.Neurofinder('./data', n=256, stop=8, subset=['01.00'])
+    ds = data.load_dataset('./data', n=256, stop=8, subset=['01.00'])
     ds = data.Torchify(ds)
     assert len(ds) == 8
 
@@ -57,3 +59,26 @@ def test_dataloader():
         assert code == ('01.00',) * 4  # 1 code for each example in the batch
         assert isinstance(x, torch.DoubleTensor)
         assert isinstance(y, torch.LongTensor)
+
+
+def test_gridsearch():
+    from sklearn.base import BaseEstimator
+    from sklearn.model_selection import GridSearchCV
+
+    # Grid search over a stub estimator with one dummy param
+    class StubEstimator(BaseEstimator):
+        def __init__(self, foo=1):
+            self.foo = foo
+        def score(self, x):
+            return self.foo
+        def fit(self, x):
+            assert isinstance(x[0]['x'], da.Array)
+            assert isinstance(x[0]['y'], np.ndarray)
+            assert isinstance(x[0]['meta'], dict)
+            return self
+
+    # The dataset should be compatible with GridSearchCV
+    ds = data.load_dataset('./data', n=256, stop=8, subset=['01.00'])
+    stub = StubEstimator(foo=1)
+    grid_stub = GridSearchCV(stub, {'foo': [1,2,3]})
+    grid_stub.fit(ds)

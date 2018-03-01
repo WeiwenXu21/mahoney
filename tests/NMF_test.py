8d73a9f63daeee0673bc57c4ac11a0c483a8e0dd
WeiwenXu21 <wx37523@uga.edu>
2018-03-01 16:35:36 -0500

    update test for NMF extraction

--- tests/NMF_test.py
+++
@@ -1,8 +0,0 @@
-import mahoney.NMF as nmf
-
-def test_NMF():
-    # All of these loads should succeed without error.
-    W,H = nmf.NMF_decomposition('./data/neurofinder.01.00')
-
-    assert W.shape == (2250, 5)  # 2250 frames, 5 clusters
-    assert H.shape == (5, 262144)  # 5 clusters, 512*512 pixels

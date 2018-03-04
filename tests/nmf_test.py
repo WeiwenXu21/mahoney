6266a64f4400d9fb4eac32f6909eb8d2a8ab12fd
WeiwenXu21 <sirjennyxu@gmail.com>
2018-03-04 14:51:13 -0500

    Fixed a test bug

--- tests/nmf_test.py
+++ tests/nmf_test.py
@@ -2,21 +2,22 @@ import mahoney.nmf as nmf
 from mahoney import io
 from scipy.misc import imread
 from glob import glob
+import numpy as np
 from numpy import array
 
 def test_nmf_decomposition():
     # All of these loads should succeed without error.
-    files = sorted(glob('./data/neurofinder.01.00/images/image*.tiff'))
+    files = sorted(glob('./data/neurofinder.00.01/images/image*.tiff'))
     video = array([imread(f) for f in files])
     k = 5
-    W,H = nmf.NMF_decomposition(video,k)
+    W,H = nmf.nmf_decomposition(video,k)
 
-    assert W.shape == (3048, 5)  # 3048 frames, 5 clusters
-    assert H.shape == (5, 262144)  # 5 clusters, 512*512 pixels
+    assert np.shape(W) == (3048, 5)  # 3048 frames, 5 clusters
+    assert np.shape(H) == (5, 262144)  # 5 clusters, 512*512 pixels
 
 def test_nmf_extraction():
     # All of these loads should succeed without error.
-    video = io.load_video('./data/neurofinder.01.00')
+    video = io.load_video('./data/neurofinder.00.01')
     k = 5
     coordinates = nmf.nmf_extraction(video,k)
     

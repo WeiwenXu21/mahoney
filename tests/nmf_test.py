83c2a4073b6cc35e6f6acb6f2f2f9cd10cebffe2
WeiwenXu21 <sirjennyxu@gmail.com>
2018-03-05 00:57:07 -0500

    Better way for testing
    - Switch to using 01.00 making testing consistent to other testing files.
    - Getting rid of hard coding of the constrains on result data
    - Reducing testing time for test_nmf_extraction() by slicing first 10 frames

--- tests/nmf_test.py
+++ tests/nmf_test.py
@@ -7,22 +7,26 @@ from numpy import array
 
 def test_nmf_decomposition():
     # All of these loads should succeed without error.
-    files = sorted(glob('./data/neurofinder.00.01/images/image*.tiff'))
+    files = sorted(glob('./data/neurofinder.01.00/images/image*.tiff'))
     video = array([imread(f) for f in files])
+    
+    numb_of_frames, dims, dims = np.shape(video)
+    
     k = 5
     W,H = nmf.nmf_decomposition(video,k)
-
-    assert np.shape(W) == (3048, 5)  # 3048 frames, 5 clusters
-    assert np.shape(H) == (5, 262144)  # 5 clusters, 512*512 pixels
+    
+    assert np.shape(W) == (numb_of_frames, k)  # for 01.00: 2250 frames, 5 clusters
+    assert np.shape(H) == (k, dims*dims)  # for 01.00: 5 clusters, 512*512 pixels
 
 def test_nmf_extraction():
     # All of these loads should succeed without error.
-    video = io.load_video('./data/neurofinder.00.01')
+    video = io.load_video('./data/neurofinder.01.00')[:10]
+    numb_of_frames, dims, dims = np.shape(video)
     k = 5
     coordinates = nmf.nmf_extraction(video,k)
     
     # number of neurons found should be over 0 and less than total pixel number
-    assert 0 < len(coordinates) < 262144
+    assert 0 < len(coordinates) < dims*dims
 
 
 

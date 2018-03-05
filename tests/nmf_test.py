49f3321aeca54bd24e979272948349781ea90ba5
Chris Barrick <cbarrick1@gmail.com>
2018-03-05 12:29:56 -0500

    Improve test speed

--- tests/nmf_test.py
+++ tests/nmf_test.py
@@ -1,20 +1,19 @@
-import mahoney.nmf as nmf
-from mahoney import io
-from scipy.misc import imread
-from glob import glob
 import numpy as np
-from numpy import array
+
+from mahoney import nmf
+from mahoney import io
+
 
 def test_nmf_decomposition():
     # All of these loads should succeed without error.
-    files = sorted(glob('./data/neurofinder.01.00/images/image*.tiff'))
-    video = array([imread(f) for f in files])
-    
+    video = io.load_video('./data/neurofinder.01.00')[:10]
+    video = video.compute()  # nmf_decomposition does not support dask arrays
+
     numb_of_frames, dims, dims = np.shape(video)
-    
+
     k = 5
     W,H = nmf.nmf_decomposition(video,k)
-    
+
     assert np.shape(W) == (numb_of_frames, k)  # for 01.00: 2250 frames, 5 clusters
     assert np.shape(H) == (k, dims*dims)  # for 01.00: 5 clusters, 512*512 pixels
 
@@ -24,10 +23,12 @@ def test_nmf_extraction():
     numb_of_frames, dims, dims = np.shape(video)
     k = 5
     coordinates = nmf.nmf_extraction(video,k)
-    
+
     # number of neurons found should be over 0 and less than total pixel number
     assert 0 < len(coordinates) < dims*dims
 
-
-
-
+    # coordinates is a list of n*2 arrays, one for each neuron.
+    # Each array is a list of pixel coordinates.
+    for array in coordinates:
+        (pixels, coords) = array.shape
+        assert coords == 2

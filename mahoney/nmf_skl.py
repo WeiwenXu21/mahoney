edd28028a5a00d876212952da74a5ba0ff615154
raj-sivakumar <raji.a.siva@gmail.com>
2018-03-05 22:07:10 -0500

    wrappers complete, just need test functions now (can't use check estimator since presumes fit takes 2d input)

---
+++ mahoney/nmf_skl.py
@@ -0,0 +1,63 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+import factorization
+import extraction
+import numpy as np
+
+class NMF_dcomp(BaseEstimator, ClassifierMixin):
+    '''Builds on the NMF decomposition with sklearn wrapper
+    This wrapper allows us to perform NMF decomposition on
+    the videos without
+    '''
+    def __init__(self, k=5):
+        '''Initializes sklearn style NMF decomposition model
+        Args:
+            x: dask array from load_dataset function in data.py
+            y: label mask if available, None if not
+            meta: meta data from load dataset including "dataset"(i.e. : 01.00)
+        '''
+        self.k = k
+    def get_params(self, deep=False):
+        return {"k": self.k}
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
+    def fit(self, X, y=None):
+        frames, dims, dims = np.shape(X)
+        full_img = X.reshape((frames,dims*dims))
+
+        # Build and fit the model. Default: k=5
+        algorithm = factorization.NMF(k=self.k)
+        W, H = algorithm.fit(full_img)
+
+        return W, H
+
+class NMF_extract(BaseEstimator, ClassifierMixin):
+    '''Builds on the NMF extraction with sklearn wrapper
+    This wrapper allows us to perform NMF decomposition on
+    the videos without
+    '''
+    def __init__(self, k=5):
+        '''Initializes sklearn style NMF extraction model
+        Args:
+            x: dask array from load_dataset function in data.py
+            y: label mask if available, None if not
+            meta: meta data from load dataset including "dataset"(i.e. : 01.00)
+        '''
+        self.k = k
+
+    def get_params(self, deep=False):
+        return {"k": self.k,}
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
+    def fit(self, X, y=None):
+        algorithm = extraction.NMF(k=self.k)
+        model = algorithm.fit(X)
+
+        # The region of neurons found.
+        regions = model.regions
+        coordinates = regions.coordinates
+
+        return coordinates

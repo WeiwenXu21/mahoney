c6f48925ab84fb54b446676f70a7fc2430baf632
Chris Barrick <cbarrick1@gmail.com>
2018-03-05 22:56:59 -0500

    Add docs, simplify the block argument

--- mahoney/unet.py
+++ mahoney/unet.py
@@ -5,7 +5,14 @@ import torch.nn as N
 import torch.nn.functional as F
 
 
-class Vgg(N.Module):
+def _get_block(name):
+    '''Maps string names to block classes.
+    '''
+    if name == 'vgg': return _Vgg
+    raise Exception(f"Unknown block type '{name}'")
+
+
+class _Vgg(N.Module):
     '''A simple block of 3x3 convolutions.
     '''
     def __init__(self, in_channel, *channels):
@@ -27,9 +34,10 @@ class Vgg(N.Module):
 class _DownBlock(N.Module):
     '''A downward block for U-Net.
     '''
-    def __init__(self, in_channel, *channels, base=Vgg):
+    def __init__(self, in_channel, *channels, block_type='vgg'):
         super().__init__()
-        self.conv = base(in_channel, *channels)
+        block = _get_block(block_type)
+        self.conv = block(in_channel, *channels)
         self.resample = N.MaxPool2d(2)
 
     def forward(self, x):
@@ -41,9 +49,10 @@ class _DownBlock(N.Module):
 class _UpBlock(N.Module):
     '''An upward block for U-Net.
     '''
-    def __init__(self, in_channel, *channels, base=Vgg):
+    def __init__(self, in_channel, *channels, block_type='vgg'):
         super().__init__()
-        self.conv = base(in_channel, *channels)
+        block = _get_block(block_type)
+        self.conv = block(in_channel, *channels)
         self.resample = N.ConvTranspose2d(channels[-1], channels[-1]//2, kernel_size=2, stride=2)
 
     def forward(self, x, skip):
@@ -55,9 +64,39 @@ class _UpBlock(N.Module):
 
 class UNet(N.Module):
     '''A fully convolutional network based on U-Net.
+
+    "U-Net: Convolutional Networks for Biomedical Image Segmentation".
+    Olaf Ronneberger, Philipp Fischer, and Thomas Brox.
+    Medical Image Computing and Computer-Assisted Intervention (MICCAI),
+    Springer, LNCS, Vol.9351: 234--241, 2015,
+    https://arxiv.org/abs/1505.04597
+
+    This architecture differs slightly from the original in that it includes
+    an extra convolution on the input which makes it easier to have a flexible
+    architecture.
     '''
-    def __init__(self, n_channels, n_classes, depth=4, size=1024, block=Vgg):
+
+    def __init__(self, n_channels, n_classes, depth=4, size=1024, block_type='vgg'):
+        '''Construct a U-Net.
+
+        Args:
+            n_channels:
+                The number of channels in the input.
+            n_classes:
+                The number of classes in the output.
+            depth:
+                The depth of the U, e.g. the number of down/up steps.
+                The default of 4 is used in the original paper.
+            size:
+                The number of channels at the very bottom.
+                The default of 1024 is used in the original paper.
+            block_type:
+                The type of convolution block. The default of 'vgg' is used in
+                the original paper. The following are supported:
+                - 'vgg': blocks of simple 3x3 convolutions with ReLU activation.
+        '''
         super().__init__()
+        block = _get_block(block_type)
 
         # Input
         c = size >> depth
@@ -67,14 +106,14 @@ class UNet(N.Module):
         # Down
         self.down = []
         for i in range(depth):
-            self.down.append(_DownBlock(c0, c, c, base=block))
+            self.down.append(_DownBlock(c0, c, c, block_type=block_type))
             c0 = c
             c *= 2
 
         # Up
         self.up = []
         for i in range(depth):
-            self.up.append(_UpBlock(c0, c, c, base=block))
+            self.up.append(_UpBlock(c0, c, c, block_type=block_type))
             c0 = c
             c //= 2
 

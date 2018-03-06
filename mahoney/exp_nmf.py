3f423e66db37b48cc769f20bc5e52ac6a3ce2bb2
raj-sivakumar <raji.a.siva@gmail.com>
2018-03-06 15:12:51 -0500

    make exp_nmf.py

---
+++ mahoney/exp_nmf.py
@@ -0,0 +1,16 @@
+from mahoney.nmf_skl import NMF_dcomp and NMF_extract
+from mahoney import preprocess
+from mahoney import io
+from scipy.misc import imread
+from glob import glob
+import numpy as np
+from sklearn import GridSearchCV
+
+model = NMF_extract(k=10)
+
+TEST_SET = [
+    '00.00.test', '00.01.test', '01.00.test', '01.01.test', '02.00.test',
+    '02.01.test', '03.00.test', '04.00.test', '04.01.test'
+]
+
+fo

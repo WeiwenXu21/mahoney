51d37ddc38ef5ff57c03ac7d79f332b4c5e256cb
raj-sivakumar <raji.a.siva@gmail.com>
2018-03-07 18:47:20 -0500

    mahoney nmf experiments update

--- mahoney/exp_nmf.py
+++ mahoney/exp_nmf.py
@@ -1,17 +1,38 @@
-from mahoney.nmf_skl import NMF_dcomp and NMF_extract
-from mahoney import preprocess
-from mahoney import io
+import mahoney.nmf as nmf
+import mahoney.io as io
+import mahoney.preprocess as preprocess
 from scipy.misc import imread
 from glob import glob
 import numpy as np
-from sklearn import GridSearchCV
-
-model = NMF_extract(k=10)
+import json
 
 TEST_SET = [
     '00.00.test', '00.01.test', '01.00.test', '01.01.test', '02.00.test',
     '02.01.test', '03.00.test', '04.00.test', '04.01.test'
 ]
 
-for vid in TEST_SET:
-    path = "../data/neurofinder{vid}"
+def std_nmf(cloud=False):
+    output = []
+    for vid in TEST_SET:
+        tempdict = {}
+        path = f'./data/neurofinder.{vid}'
+        video = io.load_video(path=path, preprocess=preprocess.normalize)
+        rois = [{"coordinates": [list(k) for k in i]} for i in nmf.nmf_extraction(video, k=5)]
+        tempdict["dataset"]=vid
+        tempdict["regions"]=rois
+        print(tempdict["regions"][0])
+        output.append(tempdict)
+    json.dumps(output)
+
+def property_nmf():
+    output = []
+    for vid in TEST_SET:
+        tempdict = {}
+        path = f'./data/neurofinder.{vid}'
+        video = io.load_video(path=path, imread=opencv, preprocess=preprocess.ed_open)
+        rois = [{"coordinates": [list(k) for k in i]} for i in nmf.nmf_extraction(video, k=5)]
+        tempdict["dataset"]=vid
+        tempdict["regions"]=rois
+        print(tempdict["regions"][0])
+        output.append(tempdict)
+    json.dumps(output)
--- mahoney/nmf.py
+++ mahoney/nmf.py
@@ -4,34 +4,34 @@ import numpy as np
 
 def nmf_decomposition(video, k=5):
     '''This is normal NMF decomposition (using thunder)
-        
+
         Args:
             video:
                 The video data to be processed.
                 shape: (frames, dims, dims)
-        
+
         Returns:
             W:
                 W is the derived features matrix with k latent features
                 shape(frames, k)
-            
+
             H:
                 H is the coefficients matrix that associates with W
                 shape(k, pixels)
         '''
     # Load in the video and flatten each frame. Shape(frames, dims*dims)
-    frames, dims, dims = np.shape(video)
-    full_img = video.reshape((frames,dims*dims))
-    
+    frames, dim1, dim2 = np.shape(video)
+    full_img = video.reshape((frames,dim1*dim2))
+
     # Build and fit the model. Default: k=5
     algorithm = factorization.NMF(k=k)
     W, H = algorithm.fit(full_img)
-    
+
     return W, H
 
 def nmf_extraction(video, k=5):
     '''This is for neuron segmentation only using NMF (using thunder)
-        
+
         Args:
             video:
                 The video data to be processed.
@@ -39,7 +39,7 @@ def nmf_extraction(video, k=5):
             k:
                 The number of components being considered in NMF.
                 Also can be interpreted as number of clustering.
-        
+
         Returns:
             coordinates:
                 Array of coordinates for each neuron.
@@ -52,5 +52,6 @@ def nmf_extraction(video, k=5):
     # The region of neurons found.
     regions = model.regions
     coordinates = regions.coordinates
-    
+
+
     return coordinates
